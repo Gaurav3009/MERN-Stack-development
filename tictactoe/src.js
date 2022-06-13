35acc53ff38887a39e21fc17590aa3346cc14008
Gaurav Pandey <57361998+Gaurav3009@users.noreply.github.com>
2022-06-13 15:33:44 +0530

    classroom work

---
+++ tictactoe/src.js
@@ -0,0 +1,18 @@
+// console.log("Hello world!");
+
+// let player = "X";
+
+// const changePlayer = () => {
+//     return player == "X"?"O":"X" ;
+// }
+
+// let boxes = Array.from(document.getElementsByClassName("box"));
+// console.log(boxes);
+
+document.getElementById("gameContainer").addEventListener("click", ()=>{
+    console.log("clicked");
+});
+
+document.getElementById("reset").addEventListener("click", ()=>{
+    console.log("clicked");
+});
